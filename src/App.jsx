3005ca675dbaff77d5d35075896bc538a8b0e33b
ThachTheStone <saiki230206@gmail.com>
2026-02-02 14:40:29 +0700

    Fix merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ import Home from "./pages/Home.jsx";
 import About from "./pages/About/About.jsx";
 import Season from "./pages/Season/Season.jsx";
 import Partner from "./pages/Partner/Partner.jsx";
-import Contact from "./pages/Contact/Contact.jsx";
+import Contact from "./pages/Contact.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import { Routes, Route } from 'react-router-dom';
-
 function App() {
   return (
     <div className="container">
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,30 +1,7 @@
-import "./Contact.css";
-
 function Contact() {
     return (
-        <div className="contact_us">
-            <div className="header-contact">
-                <h1>CONTACT US</h1>
-            </div>
+        <div>
 
-            <div className="contact_container">
-                <div className="contact_card">
-                    <img src="" alt="" />                        
-                </div>
-
-                <div className="contact_card">
-                    <img src="" alt="" />        
-                </div>
-
-                <div className="contact_card">
-                    <img src="" alt="" />        
-                </div>
-
-                <div className="contact_card">
-                    <img src="" alt="" />        
-                </div>
-
-            </div>
         </div>
     );
 }
